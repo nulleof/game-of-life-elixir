ff753b975d0e4fcf4fd3fde6a69820639d172d7c
Igor Knyazev <igor.s.knyazev@gmail.com>
2019-09-15 16:31:17 +0700

    Initial commit. Cell type was created.

---
+++ README.md
@@ -0,0 +1,3 @@
+# GameOfLife
+
+This repo reproduces tutorial from [this elixir Game of Life tutorial](https://beyondscheme.com/2016/distributed-game-of-life-in-elixir)
---
+++ lib/game_of_life/cell.ex
@@ -0,0 +1,45 @@
+defmodule GameOfLife.Cell do
+  @type cell :: {integer, integer}
+
+  @doc """
+  Should this cell be alive on next generation?
+
+  ## args
+    - alive_cells: list of neighbour cells, can be empty
+    - _alive_cell: tuple of given cell coords
+
+  """
+  @spec keep_alive?(list(cell), cell) :: bool
+  def keep_alive?(alive_cells, {x, y} = _alive_cell) do
+    num_neighbours = count_neighbours(alive_cells, x, y, 0)
+
+    case num_neighbours do
+      2 -> true
+      3 -> true
+      _ -> false
+    end
+  end
+
+  @doc """
+  Recursive count of neighbours
+  """
+  @spec count_neighbours([cell | list(cell)], integer, integer, integer) :: integer
+  defp count_neighbours([head_cell | tail_cells], x, y, count) do
+    increment =
+      case head_cell do
+        {hx, hy} when hx == x - 1 and hy == y - 1 -> 1
+        {hx, hy} when hx == x and hy == y - 1 -> 1
+        {hx, hy} when hx == x + 1 and hy == y - 1 -> 1
+        {hx, hy} when hx == x - 1 and hy == y -> 1
+        {hx, hy} when hx == x + 1 and hy == y -> 1
+        {hx, hy} when hx == x - 1 and hy == y + 1 -> 1
+        {hx, hy} when hx == x and hy == y + 1 -> 1
+        {hx, hy} when hx == x + 1 and hy == y + 1 -> 1
+        _not_neighbour -> 0
+      end
+
+    count_neighbours(tail_cells, x, y, count + increment)
+  end
+
+  defp count_neighbours([], _x, _y, count), do: count
+end
